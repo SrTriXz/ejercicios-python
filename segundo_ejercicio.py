904a63b3ac4bf68146b8b886a4e661a8b4d1f0a1
SrTriXz <torres@Emmanuel>
2023-05-06 22:18:55 -0500

    segundo ejercicio

---
+++ segundo_ejercicio.py
@@ -0,0 +1,11 @@
+
+
+while True:
+    numero_entero = int(input("introduzca un numero entero 1-9: "))
+
+    if numero_entero >= 1 and numero_entero <= 9:
+        for multiplo in range(1,100,):
+            if multiplo % numero_entero == 0:
+                print(multiplo) 
+
+
